af7182da110f5f077302fd9164b930f8b5d74043
Travis Rosenberg <travisrosenberg@Traviss-MacBook-Pro.local>
2017-03-12 17:01:27 -0400

    adds fade

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function main($center) {
+     $('.center').hide();
+   $('.center').fadeIn(1000);
+
+}
+
+function main($image) {
+	$('.image').hide();
+	$('.image').fadeIn(3000);
+}
+
+$(document).ready(main);
