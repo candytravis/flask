1df51c043cb6545f6f541a058b367b8b621e4865
candytravis <travisdylanrosenberg@gmail.com>
2017-04-02 16:06:55 -0400

    serves portrait file

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ def index():
     return render_template('index.html')
 
 @app.route('/<string:page_name>/')
-def render_portrait(page_name):
-    return render_template('portrait.html' % page_name)
+def render_static(page_name):
+    return render_template('%s.html' % page_name)
 
 
 @app.route('/static/stylesheets/style.css')
