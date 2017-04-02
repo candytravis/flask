30117008fee877382b4bd5b5cad62830e7da2257
candytravis <travisdylanrosenberg@gmail.com>
2017-04-02 15:01:06 -0400

    serves portrait file

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def render_static(page_name):
     return render_template('%s.html' % page_name)
 
 
-@app.route('/static/stylesheets/styles.css')
+@app.route('/static/stylesheets/style.css')
 def send_css(path):
     return send_from_directory('static', path)
 
