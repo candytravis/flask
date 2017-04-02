0f1a64a631f24c125e93f20bedba50a1a3e9a349
candytravis <travisdylanrosenberg@gmail.com>
2017-04-02 15:34:31 -0400

    serves portrait file

--- app.py
+++ app.py
@@ -13,20 +13,14 @@ def index():
     return render_template('index.html')
 
 @app.route('/portrait/<name>')
-def hello(name=None):
+def portrait(name=None):
     return render_template('portrait.html', name=name)
-
-@app.route('/templates/portrait.html')
-def send_html(path):
-    return send_from_directory('templates', path)
 
 
 @app.route('/static/stylesheets/style.css')
 def send_css(path):
     return send_from_directory('static', path)
 
-
-url_for('static', filename='style.css')
 
 app.static_folder = 'static'
 
